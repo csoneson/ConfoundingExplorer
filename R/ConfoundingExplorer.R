7df6ea4250c3dd861722628e1b7a0d57bc43e494
Charlotte Soneson <charlottesoneson@gmail.com>
2020-12-26 22:42:28 +0100

    Split out .generateData

--- R/ConfoundingExplorer.R
+++ R/ConfoundingExplorer.R
@@ -18,7 +18,6 @@
 #' @importFrom ComplexHeatmap columnAnnotation rowAnnotation Heatmap
 #' @importFrom shinyMatrix matrixInput
 #' @importFrom grDevices rgb
-#' @importFrom stats coefficients lm p.adjust predict residuals rnorm runif
 #'
 #' @return A shinyApp object
 #'
@@ -138,61 +137,6 @@ ConfoundingExplorer <- function() {
         )
     )
 
-    generateData <- function(nb1c1, nb1c2, nb2c1, nb2c2, fraccond, fracbatch,
-                             condeffect, batcheffect, analysisapproach, nvar,
-                             seed) {
-        set.seed(seed)
-        m <- matrix(stats::rnorm(n = (nb1c1 + nb1c2 + nb2c1 + nb2c2) * nvar,
-                                 mean = 10, sd = 2),
-                    nrow = nvar, dimnames = list(
-                        paste0("V", seq_len(nvar)),
-                        paste0("S", seq_len(nb1c1 + nb1c2 + nb2c1 + nb2c2))))
-        cond <- c(rep("C1", nb1c1), rep("C2", nb1c2),
-                  rep("C1", nb2c1), rep("C2", nb2c2))
-        batch <- c(rep("B1", nb1c1), rep("B1", nb1c2),
-                   rep("B2", nb2c1), rep("B2", nb2c2))
-        condvar <- sample(seq_len(nvar), size = fraccond * nvar)
-        batchvar <- sample(seq_len(nvar), size = fracbatch * nvar)
-        for (i in condvar) {
-            m[i, cond == "C2"] <- m[i, cond == "C2"] +
-                sign((stats::runif(1) < 0.5) - 0.5) * condeffect
-        }
-        for (i in batchvar) {
-            m[i, batch == "B2"] <- m[i, batch == "B2"] +
-                sign((stats::runif(1) < 0.5) - 0.5) * batcheffect
-        }
-        pvals <- tryCatch({
-            vapply(seq_len(nvar), function(i) {
-                if (analysisapproach == "Don't account for batch effect") {
-                    l <- stats::lm(m[i, ] ~ cond)
-                } else if (analysisapproach == "Include batch effect in model") {
-                    l <- stats::lm(m[i, ] ~ batch + cond)
-                } else if (analysisapproach == "Remove batch effect in advance") {
-                    l0 <- stats::lm(m[i, ] ~ batch)
-                    m0 <- stats::residuals(l0) + stats::coefficients(l0)["(Intercept)"]
-                    l <- stats::lm(m0 ~ cond)
-                } else if (analysisapproach == "Remove batch effect in advance,\n, accounting for condition") {
-                    l0 <- stats::lm(m[i, ] ~ cond)
-                    l1 <- stats::lm(stats::residuals(l0) ~ batch)
-                    m0 <- stats::predict(l0) + stats::residuals(l1)
-                    l <- stats::lm(m0 ~ cond)
-                } else {
-                    stop("Not implemented")
-                }
-                summary(l)$coef["condC2", "Pr(>|t|)"]
-            }, NA_real_)},
-            error = function(e) rep(NA, nvar))
-        res <- data.frame(feature = rownames(m),
-                          batchaff = seq_len(nvar) %in% batchvar,
-                          condaff = seq_len(nvar) %in% condvar,
-                          pval = pvals,
-                          padj = stats::p.adjust(pvals, method = "BH"),
-                          row.names = rownames(m))
-        annot <- data.frame(sample = colnames(m), batch = batch, cond = cond,
-                            row.names = colnames(m))
-        return(list(m = m, res = res, annot = annot))
-    }
-
     server <- function(input, output, session) {
 
         ## Display data generation description
@@ -219,16 +163,16 @@ ConfoundingExplorer <- function() {
                 shiny::need(is.numeric(input$seed),
                             "Seed must be a number")
             )
-            tmp <- generateData(nb1c1 = input$sampleSizeTable[1, 1],
-                                nb1c2 = input$sampleSizeTable[2, 1],
-                                nb2c1 = input$sampleSizeTable[1, 2],
-                                nb2c2 = input$sampleSizeTable[2, 2],
-                                fraccond = input$fraccond,
-                                fracbatch = input$fracbatch,
-                                condeffect = input$condeffect,
-                                batcheffect = input$batcheffect,
-                                analysisapproach = input$analysisapproach,
-                                nvar = 1000, seed = input$seed)
+            tmp <- .generateData(nb1c1 = input$sampleSizeTable[1, 1],
+                                 nb1c2 = input$sampleSizeTable[2, 1],
+                                 nb2c1 = input$sampleSizeTable[1, 2],
+                                 nb2c2 = input$sampleSizeTable[2, 2],
+                                 fraccond = input$fraccond,
+                                 fracbatch = input$fracbatch,
+                                 condeffect = input$condeffect,
+                                 batcheffect = input$batcheffect,
+                                 analysisapproach = input$analysisapproach,
+                                 nvar = 1000, seed = input$seed)
             datres$res <- tmp$res
             datres$m <- tmp$m
             datres$annot <- tmp$annot
---
+++ R/generateData.R
@@ -0,0 +1,71 @@
+#' Generate data
+#'
+#' @keywords internal
+#'
+#' @param nb1c1,nb1c2,nb2c1,nb2c2 Number of samples in the respective groups
+#'   (batch 1/2, condition 1/2)
+#' @param fraccond,fracbatch Fraction of variables affected by condition and
+#'   batch, respectively
+#' @param condeffect,batcheffect Condition and batch effect size, respectively
+#' @param analysisapproach Type of statistical model to fit
+#' @param nvar Number of variables
+#' @param seed Random seed
+#'
+#' @author Charlotte Soneson
+#'
+#' @importFrom stats rnorm runif lm residuals predict coefficients p.adjust
+#'
+.generateData <- function(nb1c1, nb1c2, nb2c1, nb2c2, fraccond, fracbatch,
+                         condeffect, batcheffect, analysisapproach, nvar,
+                         seed) {
+    set.seed(seed)
+    m <- matrix(stats::rnorm(n = (nb1c1 + nb1c2 + nb2c1 + nb2c2) * nvar,
+                             mean = 10, sd = 2),
+                nrow = nvar, dimnames = list(
+                    paste0("V", seq_len(nvar)),
+                    paste0("S", seq_len(nb1c1 + nb1c2 + nb2c1 + nb2c2))))
+    cond <- c(rep("C1", nb1c1), rep("C2", nb1c2),
+              rep("C1", nb2c1), rep("C2", nb2c2))
+    batch <- c(rep("B1", nb1c1), rep("B1", nb1c2),
+               rep("B2", nb2c1), rep("B2", nb2c2))
+    condvar <- sample(seq_len(nvar), size = fraccond * nvar)
+    batchvar <- sample(seq_len(nvar), size = fracbatch * nvar)
+    for (i in condvar) {
+        m[i, cond == "C2"] <- m[i, cond == "C2"] +
+            sign((stats::runif(1) < 0.5) - 0.5) * condeffect
+    }
+    for (i in batchvar) {
+        m[i, batch == "B2"] <- m[i, batch == "B2"] +
+            sign((stats::runif(1) < 0.5) - 0.5) * batcheffect
+    }
+    pvals <- tryCatch({
+        vapply(seq_len(nvar), function(i) {
+            if (analysisapproach == "Don't account for batch effect") {
+                l <- stats::lm(m[i, ] ~ cond)
+            } else if (analysisapproach == "Include batch effect in model") {
+                l <- stats::lm(m[i, ] ~ batch + cond)
+            } else if (analysisapproach == "Remove batch effect in advance") {
+                l0 <- stats::lm(m[i, ] ~ batch)
+                m0 <- stats::residuals(l0) + stats::coefficients(l0)["(Intercept)"]
+                l <- stats::lm(m0 ~ cond)
+            } else if (analysisapproach == "Remove batch effect in advance,\n, accounting for condition") {
+                l0 <- stats::lm(m[i, ] ~ cond)
+                l1 <- stats::lm(stats::residuals(l0) ~ batch)
+                m0 <- stats::predict(l0) + stats::residuals(l1)
+                l <- stats::lm(m0 ~ cond)
+            } else {
+                stop("Not implemented")
+            }
+            summary(l)$coef["condC2", "Pr(>|t|)"]
+        }, NA_real_)},
+        error = function(e) rep(NA, nvar))
+    res <- data.frame(feature = rownames(m),
+                      batchaff = seq_len(nvar) %in% batchvar,
+                      condaff = seq_len(nvar) %in% condvar,
+                      pval = pvals,
+                      padj = stats::p.adjust(pvals, method = "BH"),
+                      row.names = rownames(m))
+    annot <- data.frame(sample = colnames(m), batch = batch, cond = cond,
+                        row.names = colnames(m))
+    return(list(m = m, res = res, annot = annot))
+}
